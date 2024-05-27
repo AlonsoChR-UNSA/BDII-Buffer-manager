2c58165e856c259e016a98a4be6b16d5640bf205
Alonso <achullunquiar@unsa.edu.pe>
2024-05-26 22:11:01 -0500

    adding format

--- Classes/Block.h
+++ Classes/Block.h
@@ -1,27 +1,18 @@
+//
+// AUTOR: Alonso Chullunquia
+//
 #ifndef BLOCK_H
 #define BLOCK_H
 #include <string>
 #include <fstream>
-// Clase Block
 class Block {
 public:
-    std::string pathDisk;
-    int blockId;
-
-    // Block(const std::string& path) : pathDisk(path) {
-    //     blockId = std::stoi(path.substr(path.find("Block") + 5, path.find(".txt") - path.find("Block") - 5));
-    // }
+    std::string pathDisk; // path del archivo en disco
+    int blockId;          // id del bloque
+    Block(){}
     Block(int _blockId) {
         blockId = _blockId;
         pathDisk = "../Disk/Block" + std::to_string(blockId) + ".txt";
     }
-
-    // Block(std::ifstream& file) {
-        // std::string line;
-        // while (std::getline(file, line)) {
-        //     data += line + "\n";
-        // }
-    // }
 };
-
 #endif //BLOCK_H
